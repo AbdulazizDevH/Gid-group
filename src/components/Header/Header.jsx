f11a07114c816ab6f23335302d1b56b45d006610
AbdulazizDevH <mr.abdulaziz00791@gmail.com>
2022-12-05 20:36:12 +0500

    Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header pt-96 pb-60'>
+      <div className="container">
+        <h1 className='text-[#F2F2F2] text-9xl leading-[9.4rem] text-center '>Ремонт вашей мечты</h1>
+        <div className='mt-52 text-center text-white'>
+          <a href="#" className='header__btn w-[348px] py-[24px] mr-10 inline-block text-[2.4rem] leading-[2.8rem] bg-[#111111E5] hover:bg-[]'>Проекты</a>
+          <a href="#" className='header__btn w-[348px] py-[24px] ml-10 inline-block text-[2.4rem] leading-[2.8rem] bg-[#111111E5]'>Расчитать стоимость</a>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import Logo from '../../assets/header/Logo.png';
+import phone from '../../assets/header/phone.png';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="fixed left-0 right-0 navbar py-[14px] bg-[#000000CC]">
+      <div className="container flex justify-between items-center">
+        <a href="/">
+          <img src={phone} />
+        </a>
+        <a href="/">
+          <img src={Logo} />
+        </a>
+        <a href="/">
+          <img src={phone} />
+        </a>
+      </div>
+    </div>
+  );
 }
